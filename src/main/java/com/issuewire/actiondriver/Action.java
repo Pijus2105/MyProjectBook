5e3268eb6f23c6d1a01dcd76c27b93cd6aa88e8d
PERFECT <PERFECT@DESKTOP-HCSN4U9>
2022-11-06 23:50:44 +0530

    7th

--- src/main/java/com/issuewire/actiondriver/Action.java
+++ src/main/java/com/issuewire/actiondriver/Action.java
@@ -1,15 +1,27 @@
 package com.issuewire.actiondriver;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.issuewire.actioninterface.ActionInterface;
@@ -635,5 +647,221 @@ public class Action extends MainClass implements ActionInterface  {
 		}
 		 return num;
 		
+	}
+	
+	
+	
+	
+	
+	
+	public int getRowCount(WebElement table) {
+		List<WebElement> rows = table.findElements(By.tagName("tr"));
+		int number = rows.size() - 1;
+		
+		return number;
+		/**
+		 * Verify alert present or not
+		 * 
+		 * @return: Boolean (True: If alert preset, False: If no alert)
+		 * 
+		 */
+	}
+	
+	
+	
+	public boolean switchAlert(WebDriver drive) {
+		boolean flagAlert = false;
+		
+		Alert alert = null;
+		
+		try {
+			alert = driver.switchTo().alert();
+			alert.accept();
+			flagAlert = true;
+		} catch (NoAlertPresentException ex) {
+			ex.printStackTrace();
+		} finally {
+			if(flagAlert) {
+				System.out.println("The Alert is handled successfully");
+			}else {
+				System.out.println("There was no alert to handle");
+			}
+		}
+		
+		return flagAlert;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	public boolean launchURL(WebDriver driver, String URL) {
+		boolean flagURL = false;
+		
+		try {
+			driver.navigate().to(URL);
+			
+			flagURL = true;
+			return flagURL;
+			
+		} catch (Exception e) {
+			flagURL = false;
+			return flagURL;
+		} finally {
+			if(flagURL) {
+				System.out.println("Launch"+URL+"is successfully");
+			}else {
+				System.out.println("Launch"+URL+"is not successfully");
+			}
+		}
+			
+		
+	}
+	
+	
+	
+	
+	
+	
+	public boolean alertPresent (WebDriver drive) {
+		
+		try {
+			driver.switchTo().alert();
+			return true;
+		}catch (Exception e) {
+			return false;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	public String getTitle(WebDriver driver) {
+		boolean flagTitle =  false;
+	
+		 
+		 String text = driver.getTitle();
+		 
+		 if(flagTitle) {
+			 System.out.println("Current URL is: \""+text+"\"");
+		 }
+		 return text;
+		 
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	public boolean click1(WebElement locator, String locatorName) {
+		boolean flagClick1 = false;
+     try {
+    	 locator.click();
+    	
+    	 flagClick1 = true;
+    	 return flagClick1;
+     } catch (Exception e) {
+    	 flagClick1= false;
+    	 return flagClick1;
+     } finally {
+    	 if(flagClick1) {
+    		 System.out.println("Click the locator" +locatorName+"Successfully");
+    	 }else {
+    		 System.out.println("Click the locator" +locatorName+"not Successfully");
+    	 }
+     }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public void fluentWait(WebDriver driver,WebElement element, int timeOut) {
+	    Wait<WebDriver> wait = null;
+	    try {
+	        wait = new FluentWait<WebDriver>((WebDriver) driver).withTimeout(Duration.ofSeconds(20)).pollingEvery(Duration.ofSeconds(2)).ignoring(Exception.class);
+	        wait.until(ExpectedConditions.visibilityOf(element));
+	        element.click();
+	    }catch(Exception e) {
+	    }
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void implicitWait(WebDriver driver, int timeOut) {
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	
+	
+	
+	
+	public void Explicitwait(WebDriver driver, WebElement element,  int timeout) {
+		WebDriverWait wait = new WebDriverWait (driver, timeout);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void pageLoadTimeunit(WebDriver driver, int timeout) {
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+	}
+	
+	
+	
+	
+	
+	
+	public String screenShot(WebDriver driver, String filename) {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "\\ScreenShots\\" + filename + "_" + dateName + ".png";
+
+		try {
+			FileUtils.copyFile(source, new File(destination));
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		// This new path for jenkins
+		String newImageString = "http://localhost:8082/job/MyStoreProject/ws/MyStoreProject/ScreenShots/" + filename + "_"
+				+ dateName + ".png";
+		return newImageString;
+	}
+	
+	
+	
+	
+	
+	
+	
+	public String getCurrentTime() {
+		String currentDate = new SimpleDateFormat("yyyy-MM-dd-hhmmss").format(new Date());
+		return currentDate;
 	}
 }
