2a214cb0802b3d996c687facc92772ed1dc8618a
PERFECT <PERFECT@DESKTOP-HCSN4U9>
2022-11-20 11:03:10 +0530

    7

--- src/main/java/com/issuewire/pageobjects/LogIn.java
+++ src/main/java/com/issuewire/pageobjects/LogIn.java
@@ -3,6 +3,9 @@
  */
 package com.issuewire.pageobjects;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.issuewire.base.MainClass;
 
 /**
@@ -11,9 +14,46 @@ import com.issuewire.base.MainClass;
  */
 public class LogIn extends MainClass  {
 	
+	//Test-01
+	@FindBy(xpath = ("//a[@title='login']"))
+	WebElement LogInbutton;
 	
+	//Test-02
+	@FindBy(xpath = ("//input[@id='submit-btn1']"))
+	WebElement SubmitButton;
 	
-	
-	
+	    //Test-03 & 4 & 5
+		@FindBy(xpath = ("//input[@id='email']"))
+		WebElement inValidUser;
 
+		//Test-03 & 4 & 5
+		@FindBy(xpath = ("//input[@id='password']"))
+		WebElement inValidPassword;
+		
+		//Test-06
+		@FindBy(xpath = ("//input[@id='email']"))
+		WebElement ValidUser;
+		
+		//Test-07
+		@FindBy(xpath = ("//input[@id='password']"))
+		WebElement ValidPassword;
+		
+		//Test-10
+		@FindBy(xpath = ("//input[@id='email']"))
+		WebElement iValidUser;
+
+		//Test-10
+		@FindBy(xpath = ("//input[@id='password']"))
+		WebElement nValidPassword;
+		
+		//popup Test - 11 &12
+		
+		//TC-014
+		@FindBy(linkText =("Forgot Password"))
+        WebElement UserForGotPassword;
+		
+		//TC-015
+		@FindBy(linkText =("Signup"))
+        WebElement UserSignUp;
+		
 }
